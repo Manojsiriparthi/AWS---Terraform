38dd64b5d20615c6441bb08ab7005abd2c57e64a
Manoj kumar Siriparthi <manojs.devops1@gmail.com>
2024-12-28 01:22:03 +0000

    added eks cluster along with node & iam roles

---
+++ modules/EKS/main.tf
@@ -0,0 +1,80 @@
+resource "aws_eks_cluster" "main" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = var.subnet_ids
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster_role]
+  
+  tags = {
+    Name        = "EKS-${var.cluster_name}"
+    Environment = var.environment
+  }
+}
+
+resource "aws_eks_node_group" "main" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "${var.node_group_name}"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = var.subnet_ids
+
+  scaling_config {
+    desired_size = var.node_count
+    min_size     = var.node_min_size
+    max_size     = var.node_max_size
+  }
+
+  instance_types = ["t2.micro"]
+
+  tags = {
+    Name        = "EKS-NodeGroup-${var.node_group_name}"
+    Environment = var.environment
+  }
+}
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = data.aws_iam_policy_document.eks_cluster_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "eks_cluster_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_role" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-role"
+
+  assume_role_policy = data.aws_iam_policy_document.eks_node_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "eks_node_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_role" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
---
+++ modules/EKS/variable.tf
@@ -0,0 +1,40 @@
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs"
+  type        = list(string)
+}
+
+variable "environment" {
+  description = "Environment (dev, staging, prod)"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "node_group_name" {
+  description = "Name of the EKS node group"
+  type        = string
+}
+
+variable "node_count" {
+  description = "Desired number of worker nodes"
+  type        = number
+}
+
+variable "node_min_size" {
+  description = "Minimum number of worker nodes"
+  type        = number
+}
+
+variable "node_max_size" {
+  description = "Maximum number of worker nodes"
+  type        = number
+}
+
