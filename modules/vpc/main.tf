e27f4beb57b64f42b72e1e8adf405b146721d119
Manoj kumar Siriparthi <manojs.devops1@gmail.com>
2024-12-28 01:03:27 +0000

    add vpc terraform files

---
+++ modules/vpc/main.tf
@@ -0,0 +1,150 @@
+# VPC 
+
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name        = "VPC-${var.environment}"
+    Environment = var.environment
+  }
+}
+
+# SUBNET-PUBLIC
+
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  map_public_ip_on_launch = true
+  availability_zone       = element(var.availability_zones, count.index)
+
+  tags = {
+    Name        = "Public-Subnet-${var.environment}-${count.index}"
+    Environment = var.environment
+  }
+}
+
+# SUBNET-PRIVATE
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = element(var.availability_zones, count.index)
+
+  tags = {
+    Name        = "Private-Subnet-${var.environment}-${count.index}"
+    Environment = var.environment
+  }
+}
+
+# INTERNET GATEWAY
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "IGW-${var.environment}"
+    Environment = var.environment
+  }
+}
+
+# ROUTETABLE-PUBLIC
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name        = "Public-RouteTable-${var.environment}"
+    Environment = var.environment
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+# NATGATEWAY
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name        = "NAT-Gateway-${var.environment}"
+    Environment = var.environment
+  }
+}
+
+resource "aws_eip" "nat" {
+  vpc = true
+
+  tags = {
+    Name        = "NAT-EIP-${var.environment}"
+    Environment = var.environment
+  }
+}
+
+# ROUTETABLE-PRIVATE
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
+  tags = {
+    Name        = "Private-RouteTable-${var.environment}"
+    Environment = var.environment
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnet_cidrs)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+# SECUIRTY GROUP
+
+resource "aws_security_group" "main" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "SG-${var.environment}"
+    Environment = var.environment
+  }
+}
+
